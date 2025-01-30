c60c0d0e27df4bd27b1b7f40f52acec0200ef888
Benjamin Goisser <benjamin.goisser.bg@gmail.com>
2025-01-30 23:36:08 +0100

    Neovim fix copilot disable on startup

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,6 @@
 -- Disable copilot on startup
-vim.api.nvim_create_autocmd("VimEnter", { command = "Copilot disable" })
-vim.g.copilot_enabled = false
+vim.api.nvim_create_autocmd("VimEnter", { callback = function()
+	print("Copilot disabled")
+	vim.cmd("Copilot disable")
+	vim.g.copilot_enabled = false
+end })
