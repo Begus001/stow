d30abad09c84038abebbafb38123af1f5961f7f1
Benjamin Goisser <benjamin.goisser.bg@gmail.com>
2025-04-15 21:12:21 +0200

    Configure neovide

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -27,3 +27,33 @@ vim.g.root_spec = {
   "lua",
   "cwd",
 }
+
+
+if vim.g.neovide then
+
+  vim.g.snacks_animate = false
+
+  vim.g.neovide_cursor_animation_length = 0.1
+  vim.g.neovide_cursor_trail_size = 0.70
+  vim.g.neovide_cursor_animate_in_insert_mode = true
+  vim.g.neovide_cursor_animate_command_line = true
+  vim.g.neovide_cursor_unfocused_outline_width = 0.05
+  vim.g.neovide_cursor_smooth_blink = true
+
+  vim.g.neovide_scroll_animation_length = 0.20
+  vim.g.neovide_scroll_animation_far_lines = 1
+
+  vim.g.neovide_opacity = 1.00
+  vim.g.neovide_normal_opacity = 1.00
+
+  vim.g.neovide_floating_blur_amount_x = 10.0
+  vim.g.neovide_floating_blur_amount_y = 10.0
+
+
+  vim.g.neovide_position_animation_length = 0.25
+
+  vim.g.neovide_floating_corner_radius = 0.4
+
+  vim.g.neovide_scale_factor = 0.80
+
+end
