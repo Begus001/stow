b92895c31e8b2eab415ad3659e93665689351eb5
Benjamin Goisser <benjamin.goisser.bg@gmail.com>
2025-04-16 14:57:01 +0200

    Add neovide scale keys

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -28,13 +28,11 @@ vim.g.root_spec = {
   "cwd",
 }
 
-
 if vim.g.neovide then
-
   vim.g.snacks_animate = false
 
   vim.g.neovide_cursor_animation_length = 0.1
-  vim.g.neovide_cursor_trail_size = 0.70
+  vim.g.neovide_cursor_trail_size = 1.00
   vim.g.neovide_cursor_animate_in_insert_mode = true
   vim.g.neovide_cursor_animate_command_line = true
   vim.g.neovide_cursor_unfocused_outline_width = 0.05
@@ -49,11 +47,27 @@ if vim.g.neovide then
   vim.g.neovide_floating_blur_amount_x = 10.0
   vim.g.neovide_floating_blur_amount_y = 10.0
 
-
   vim.g.neovide_position_animation_length = 0.25
 
   vim.g.neovide_floating_corner_radius = 0.4
 
-  vim.g.neovide_scale_factor = 0.80
+  local scale = 0.80
+  local step = 1.05
+  vim.g.neovide_scale_factor = scale
+
+  vim.keymap.set({ "n", "i", "x" }, "<C-+>", function()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * step
+    print(vim.g.neovide_scale_factor)
+  end)
+
+  vim.keymap.set({ "n", "i", "x" }, "<C-->", function()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor / step
+    print(vim.g.neovide_scale_factor)
+  end)
+
+  vim.keymap.set({ "n", "i", "x" }, "<C-0>", function()
+    vim.g.neovide_scale_factor = scale
+    print(vim.g.neovide_scale_factor)
+  end)
 
 end
