b6a2ad51ce01020b7c00d5b7425463b8d5546b5e
Benjamin Goisser <benjamin.goisser.bg@gmail.com>
2025-04-23 14:14:59 +0200

    Improve command window ctrl+c behavior in neovim

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -5,3 +5,11 @@ vim.api.nvim_create_autocmd("BufEnter", {
     vim.b.copilot_enabled = false
   end,
 })
+
+vim.api.nvim_create_augroup("CmdwinMappings", { clear = true })
+vim.api.nvim_create_autocmd("CmdwinEnter", {
+  group = "CmdwinMappings",
+  callback = function()
+    vim.keymap.set({ "i", "v" }, "<C-c>", "<Esc>", { buffer = true, remap = true })
+  end,
+})
