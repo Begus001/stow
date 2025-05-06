421ace17a332d8d7c36199fa9a5ffa3fb86f5d0f
Benjamin Goisser <benjamin.goisser.bg@gmail.com>
2025-05-06 21:55:11 +0200

    neovim: reconfigure clangd lsp

--- neovim/.config/nvim/lua/plugins/lsp-config.lua
+++ neovim/.config/nvim/lua/plugins/lsp-config.lua
@@ -2,12 +2,25 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      clangd = {
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=bundled",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+          "--header-insertion-decorators",
+          "--compile-commands-dir=build",
+        },
+      },
       qmlls = {
         cmd = { "qmlls6" },
       },
       mesonlsp = {
         cmd = { "mesonlsp", "--lsp", "--full" },
-      }
+      },
     },
   },
 }
