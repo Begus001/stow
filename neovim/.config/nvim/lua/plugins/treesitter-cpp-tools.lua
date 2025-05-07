a08ecba76f36d5b9f07cafc0f01c8ac23b375b02
Benjamin Goisser <benjamin.goisser.bg@gmail.com>
2025-05-07 16:14:53 +0200

    neovim: switch to my own forked version of nvim-treesitter-cpp-tools

--- neovim/.config/nvim/lua/plugins/treesitter-cpp-tools.lua
+++ neovim/.config/nvim/lua/plugins/treesitter-cpp-tools.lua
@@ -1,5 +1,5 @@
 return {
-  "Badhi/nvim-treesitter-cpp-tools",
+  "Begus001/nvim-treesitter-cpp-tools",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   -- Optional: Configuration
   opts = function()
@@ -16,7 +16,7 @@ return {
         },
         --[[
                 <your impl function custom command name> = {
-                    output_handle = function (str, context) 
+                    output_handle = function (str, context)
                         -- string contains the class implementation
                         -- do whatever you want to do with it
                     end
@@ -29,3 +29,37 @@ return {
   -- End configuration
   config = true,
 }
+
+
+-- DEVELOPMENT
+
+-- return {
+--   dir = "/home/main/src/nvim-treesitter-cpp-tools",
+--   dependencies = { "nvim-treesitter/nvim-treesitter" },
+--   opts = function()
+--     local options = {
+--       preview = {
+--         quit = "<esc>", -- optional keymapping for quit preview
+--         accept = "<tab>", -- optional keymapping for accept preview
+--       },
+--       header_extension = "h", -- optional
+--       source_extension = "cpp", -- optional
+--       custom_define_class_function_commands = { -- optional
+--         TSCppImplWrite = {
+--           output_handle = require("nt-cpp-tools.output_handlers").get_add_to_cpp(),
+--         },
+--         --[[
+--               <your impl function custom command name> = {
+--                   output_handle = function (str, context) 
+--                       -- string contains the class implementation
+--                       -- do whatever you want to do with it
+--                   end
+--               }
+--               ]]
+--       },
+--     }
+--     return options
+--   end,
+--   -- End configuration
+--   config = true,
+-- }
